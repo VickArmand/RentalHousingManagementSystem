b5b9adaaf90271d5e415273c67a0982220a12998
VickArmand <victormaina1962@gmail.com>
2024-02-28 10:41:15 +0300

    Working on Email and Password Login

--- app/src/main/java/com/example/rentalhousingmanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/rentalhousingmanagementsystem/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.rentalhousingmanagementsystem;
 import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.ui.AppBarConfiguration;
-
+import com.example.rentalhousingmanagementsystem.controller.AuthController;
 import com.example.rentalhousingmanagementsystem.databinding.ActivityMainBinding;
 
 
@@ -10,13 +15,30 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-
+    private EditText emailTextView, passwordTextView;
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        btnLogin = findViewById(R.id.btnlogin);
+        emailTextView = findViewById(R.id.txtemail);
+        passwordTextView = findViewById(R.id.txtpassword);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password;
+                email = emailTextView.getText().toString();
+                password = passwordTextView.getText().toString();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        new AuthController().LoginController(email, password);
+                    }
+                });
+            }
+        });
     }
 }
--- app/src/main/java/com/example/rentalhousingmanagementsystem/model/Auth.java
+++ app/src/main/java/com/example/rentalhousingmanagementsystem/model/Auth.java
@@ -2,7 +2,9 @@ package com.example.rentalhousingmanagementsystem.model;
 
 import static android.content.ContentValues.TAG;
 
+import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
@@ -20,16 +22,15 @@ import java.util.concurrent.Executor;
 public class Auth {
     private FirebaseAuth mAuth;
     protected FirebaseUser currentUser;
-    protected Auth()
+    private Context context;
+    public Auth()
     {
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
     }
 
-    protected FirebaseUser Login(HashMap data){
-        String email = (String) data.get("email");
-        String password = (String) data.get("password");
+    public void Login(String email, String password){
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -37,64 +38,61 @@ public class Auth {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithCredential:success");
+                            Toast.makeText(context,"Login success", Toast.LENGTH_SHORT);
                             currentUser = mAuth.getCurrentUser();
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.d(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(context,"Incorrect Email Address or Password", Toast.LENGTH_SHORT);
                         }
                     }
                 });
-        return currentUser;
     }
-    protected FirebaseUser GoogleLogin(){
-        Object signInRequest = BeginSignInRequest.builder().setGoogleIdTokenRequestOptions(GoogleIdTokenRequestOptions.builder()
-                .setSupported(true)
-                // Your server's client ID, not your Android client ID.
-                .setServerClientId(getString(R.string.default_web_client_id))
-                // Only show accounts previously used to sign in.
-                .setFilterByAuthorizedAccounts(true)
-                .build());
-        SignInCredential googleCredential = oneTapClient.getSignInCredentialFromIntent(data);
-        String idToken = googleCredential.getGoogleIdToken();
-        if (idToken !=  null) {
-            // Got an ID token from Google. Use it to authenticate with Firebase.
-            AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
-            mAuth.signInWithCredential(firebaseCredential)
-                    .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                // Sign in success, update UI with the signed-in user's information
-                                Log.d(TAG, "signInWithCredential:success");
-                                currentUser = mAuth.getCurrentUser();
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Log.d(TAG, "signInWithCredential:failure", task.getException());
-                            }
-                        }
-                    });
-        }
-        return currentUser;
+    public void GoogleLogin(){
+//        Object signInRequest = BeginSignInRequest.builder().setGoogleIdTokenRequestOptions(GoogleIdTokenRequestOptions.builder()
+//                .setSupported(true)
+//                // Your server's client ID, not your Android client ID.
+//                .setServerClientId(getString(R.string.default_web_client_id))
+//                // Only show accounts previously used to sign in.
+//                .setFilterByAuthorizedAccounts(true)
+//                .build());
+//        SignInCredential googleCredential = oneTapClient.getSignInCredentialFromIntent(data);
+//        String idToken = googleCredential.getGoogleIdToken();
+//        if (idToken !=  null) {
+//            // Got an ID token from Google. Use it to authenticate with Firebase.
+//            AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
+//            mAuth.signInWithCredential(firebaseCredential)
+//                    .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<AuthResult> task) {
+//                            if (task.isSuccessful()) {
+//                                // Sign in success, update UI with the signed-in user's information
+//                                Log.d(TAG, "signInWithCredential:success");
+//                                currentUser = mAuth.getCurrentUser();
+//                            } else {
+//                                // If sign in fails, display a message to the user.
+//                                Log.d(TAG, "signInWithCredential:failure", task.getException());
+//                            }
+//                        }
+//                    });
+//        }
     }
-    protected void Logout(){
+    public void Logout(){
         FirebaseAuth.getInstance().signOut();
     }
-    protected boolean[] Register(HashMap data) {
+    public void Register(HashMap data) {
         String email = (String) data.get("email");
         String password = (String) data.get("password");
-        final boolean[] success = {false};
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Log.d(TAG, "signInWithCredential:success");
-                            success[0] = true;
                         } else {
                             Log.d(TAG, "signInWithCredential:failure", task.getException());
                         }
                 }
         });
-        return success;
     }
 }
